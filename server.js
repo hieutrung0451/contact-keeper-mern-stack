af897af83fabd52acb621e2c6388936071e484f6
trunghieu <trunghieu0451@gmail.com>
2023-03-02 14:54:15 +0700

    add routes

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const contacts = require("./routes/contacts");
 
 const app = express();
 
 app.get("/", (req, res) => res.json({ msg: "Welcome to contact keeper API" }));
+
+// Define Routes
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/contacts", contacts);
 
 const PORT = process.env.PORT || 5000;
 
